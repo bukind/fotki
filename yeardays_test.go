534e7950f7e418ae64a180c32994d8af02a353f6
Dmitry Bukin <bukin.dima@gmail.com>
2016-02-11 15:27:29 +1100

    Test added

---
+++ yeardays_test.go
@@ -0,0 +1,29 @@
+package fotki
+
+import (
+    "fmt"
+    "os"
+    "path/filepath"
+	"strings"
+    "testing"
+	"time"
+)
+
+
+func TestMakedir(t *testing.T) {
+    DryRun = false
+	Verbose = true
+
+    temp := os.TempDir()
+	tstr := time.Now().Format(time.RFC3339Nano)
+	tstr = strings.Replace(tstr, ":", ".", -1)
+	dir := filepath.Join(temp, tstr, "hello", "world")
+	fmt.Println("making a directory", dir)
+	info, err := makedir(dir)
+	if err != nil {
+	    t.Fatal("cannot create %s: %s", dir, err.Error())
+	}
+	if ! info.IsDir() || info.Name() != "world" {
+	    t.Fatal("wrong parameters of the directory, %v", info)
+	}
+}
