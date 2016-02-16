786db4b2dff6f5af199dea9a1ade9e295ee26546
Dmitry Bukin <bukin.dima@gmail.com>
2016-02-16 13:52:53 +1100

    Add a test for path manipulation

---
+++ tests/test03.go
@@ -0,0 +1,17 @@
+// some path manipulation
+package main
+
+import (
+    "fmt"
+	"path/filepath"
+)
+
+func main() {
+    path := "hello/world.ext"
+    fmt.Println(path)
+	fmt.Println(filepath.Base(path))
+	fmt.Println(path[:len(path)-len(filepath.Ext(path))])
+	noex := "bye/life"
+	fmt.Println(noex)
+	fmt.Println(noex[:len(noex)-len(filepath.Ext(noex))])
+}
