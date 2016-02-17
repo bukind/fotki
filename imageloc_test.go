850dfd169f552a2783ec53af9bd31aded337d627
Dmitry Bukin <bukin.dima@gmail.com>
2016-02-17 11:45:59 +1100

    Add a test for GetImageKind

---
+++ imageloc_test.go
@@ -0,0 +1,28 @@
+package fotki
+
+import "testing"
+
+
+func TestGetImageKind(t *testing.T) {
+    type testRes struct {
+	    path string
+		res ImageKind
+	}
+
+	tests := []testRes{
+	    {"", NoImage},
+		{".jpg", IsImage},
+		{"A.JpG", IsImage},
+		{"B.JPEG", IsImage},
+		{"C.JPEG_", NoImage},
+		{"d.mp4", IsMovie},
+		{"e.avi", IsMovie},
+	}
+
+	for _, tr := range tests {
+	    res := GetImageKind(tr.path)
+		if res != tr.res {
+		    t.Errorf("Wrong GetImageKind(%s) = %v, must be %v", tr.path, res, tr.res)
+		}
+	}
+}
