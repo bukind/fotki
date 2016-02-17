0034261e303992a11e19a2776aa8571e2885af48
Dmitry Bukin <bukin.dima@gmail.com>
2016-02-17 16:05:51 +1100

    go fmt

--- imageloc_test.go
+++ imageloc_test.go
@@ -1,8 +1,8 @@
 package fotki
 
 import (
-    "os"
-    "testing"
+	"os"
+	"testing"
 )
 
 func TestGetImageKind(t *testing.T) {
@@ -29,36 +29,35 @@ func TestGetImageKind(t *testing.T) {
 	}
 }
 
-
 func TestImageLocExtractDate(t *testing.T) {
-    type testRes struct {
-	    path string
+	type testRes struct {
+		path string
 		date ImageDate
 	}
 
-    tests := []testRes{
-	    {"testdata/tmp.jpg", ImageDate{2012, 12, 10}},
-	    {"testdata/img2016_02_17.jpg", ImageDate{2016, 02, 17}},
+	tests := []testRes{
+		{"testdata/tmp.jpg", ImageDate{2012, 12, 10}},
+		{"testdata/img2016_02_17.jpg", ImageDate{2016, 02, 17}},
 		{"testdata/1455684141000.jpg", ImageDate{2016, 02, 17}},
 	}
 	for _, tr := range tests {
-	    osinfo, err := os.Stat(tr.path)
+		osinfo, err := os.Stat(tr.path)
 		if err != nil {
-		    t.Errorf("image is not found %s\n", tr.path)
+			t.Errorf("image is not found %s\n", tr.path)
 			continue
 		}
-	    loc := &ImageLoc{tr.path, osinfo, GetImageKind(tr.path)}
+		loc := &ImageLoc{tr.path, osinfo, GetImageKind(tr.path)}
 		info, err := loc.ExtractDate()
 		if err != nil {
-		    t.Errorf("could not get the date for %s: %s\n", tr.path, err.Error())
+			t.Errorf("could not get the date for %s: %s\n", tr.path, err.Error())
 			continue
 		}
-		if ! os.SameFile(loc.info, info.info) {
-		    t.Errorf("different info for %s\n", tr.path)
+		if !os.SameFile(loc.info, info.info) {
+			t.Errorf("different info for %s\n", tr.path)
 			continue
 		}
 		if info.date != tr.date {
-		    t.Errorf("wrong date found for %s: %v != %v\n", tr.path, info.date, tr.date)
+			t.Errorf("wrong date found for %s: %v != %v\n", tr.path, info.date, tr.date)
 			continue
 		}
 	}
