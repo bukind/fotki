958b50ca7a2e22e6f03e11eb919f42d347af144a
Dmitry Bukin <bukin.dima@gmail.com>
2016-02-17 16:28:07 +1100

    go fmt

--- tests/test02.go
+++ tests/test02.go
@@ -2,33 +2,31 @@
 package main
 
 import (
-    "fmt"
-    "os"
+	"fmt"
+	"os"
 )
 
-
 func IsTTY() bool {
-    if info, err := os.Stdout.Stat(); err != nil {
-        fmt.Fprintf(os.Stderr, "cannot stat stdout: %s\n", err.Error())
-        return false
-    } else if (info.Mode() & os.ModeDevice) != 0 {
-        return true
-    }
-    return false
+	if info, err := os.Stdout.Stat(); err != nil {
+		fmt.Fprintf(os.Stderr, "cannot stat stdout: %s\n", err.Error())
+		return false
+	} else if (info.Mode() & os.ModeDevice) != 0 {
+		return true
+	}
+	return false
 }
 
-
 func main() {
-    istty := IsTTY()
-    fmt.Printf("istty = %v\n", istty)
-    total := 10
-    for i := 0; i < total; i++ {
-        if istty {
-            fmt.Printf("\r%d/%d", i, total)
-        } else {
-		    fmt.Printf("%d/%d\n", i, total)
+	istty := IsTTY()
+	fmt.Printf("istty = %v\n", istty)
+	total := 10
+	for i := 0; i < total; i++ {
+		if istty {
+			fmt.Printf("\r%d/%d", i, total)
+		} else {
+			fmt.Printf("%d/%d\n", i, total)
 		}
 		time.Sleep()
-    }
-    fmt.Println()
+	}
+	fmt.Println()
 }
--- tests/test03.go
+++ tests/test03.go
@@ -2,13 +2,13 @@
 package main
 
 import (
-    "fmt"
+	"fmt"
 	"path/filepath"
 )
 
 func main() {
-    path := "hello/world.ext"
-    fmt.Println(path)
+	path := "hello/world.ext"
+	fmt.Println(path)
 	fmt.Println(filepath.Base(path))
 	fmt.Println(path[:len(path)-len(filepath.Ext(path))])
 	noex := "bye/life"
